bd484480d8efe07730d468a8b64fadd5342716b0
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-04-07 17:03:25 +0530

    small update

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,7 +4,7 @@ function Background() {
   return (
     <div className='absolute w-full h-screen z-[2]'>
       <h1 className='absolute text-[11vw] leading-none tracking-tighter top-1/2 left-1/2 -translate-y-[50%] -translate-x-[50%] text-zinc-700'>DOCS.</h1>
-<div className='flex justify-center text-zinc-600 text-md w-full py-10 font-semibold'>DOCUMENTS..</div>
+<div className='flex justify-center text-zinc-600 text-md w-full py-10 font-semibold'>BY VIKRANTH ..</div>
     </div>
   )
 }
