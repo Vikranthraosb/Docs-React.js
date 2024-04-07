8e3fd66144fdd7b9afc3082a9d842442da5276c2
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-04-07 15:47:36 +0530

    card component sucessfully completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,22 +5,23 @@ import { FaCloudDownloadAlt } from "react-icons/fa";
 function Card() {
   return (
     <div>
-       <div className=' relative w-60 h-72 bg-zinc-900/90 rounded-[40px] text-white p-5 overflow-hidden'>
-        <div className='p-5 flex-col items-center justify-between'>
-      <span><FaFileCode />
-        </span> 
-<p className='text-xs mt-5 leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-<div className='flex justify-between items-center p-[20px] px-10'> 
+       <div className=' relative w-60 h-72 bg-zinc-900/90 rounded-[40px] text-white overflow-hidden'>
+        <div className='flex flex-col items-start justify-center gap-5 p-[20px] py-[30px]'>
+      <div><FaFileCode size="1.5rem"/>
+         </div>
+         <div><p className='text-xs mt-5 leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing.</p></div>
+
+<div className=' relative flex justify-between items-center py-[8px] w-full mt-[15px]'> 
         <h5>.9MB</h5>
-        <span className='w-5 h-5 bg-green-800 rounded-full flex items-center justify-center'>
-        <FaCloudDownloadAlt size=".7rem" color='#fff'/>
+        <span className='w-[1.6rem] h-[1.6rem] bg-zinc-500 rounded-full flex items-center justify-center'>
+        <FaCloudDownloadAlt size=".85rem" color='#fff'/>
 
         </span>
 
     </div>
     </div>
-<div className='footer absolute bottom-0 bg-sky-700 left-0 w-full h-[60px]'>
-    <h3></h3>
+<div className='footer absolute bottom-0 bg-sky-700 left-0 w-full h-[60px] flex items-center justify-center'>
+    <h3 className='font-semibold text-zinc-300'>Download Now</h3>
 </div>
        </div>
     </div>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from './Card'
 function Foreground() {
+  
   return (
     <div>
             <div className='w-full h-screen fixed z-[3] fixed top-0 left-0'>
