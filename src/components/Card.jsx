8d08171f926d0c74d13cfa372e3ba6e5a98247f4
vikranthraosb <vikranthraosbvikranthrao@gmail.com>
2024-04-07 16:36:03 +0530

    cards mapped succesfully

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,28 +1,34 @@
 import React from 'react'
 import { FaFileCode } from "react-icons/fa";
 import { FaCloudDownloadAlt } from "react-icons/fa";
+import { IoCloseOutline } from "react-icons/io5";
 
-function Card() {
+
+function Card({data}) {
   return (
     <div>
        <div className=' relative w-60 h-72 bg-zinc-900/90 rounded-[40px] text-white overflow-hidden'>
-        <div className='flex flex-col items-start justify-center gap-5 p-[20px] py-[30px]'>
+        <div className='flex flex-col items-start justify-center gap-5 p-[20px] py-[30px] pt-[50px]'>
       <div><FaFileCode size="1.5rem"/>
          </div>
-         <div><p className='text-xs mt-5 leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing.</p></div>
+         <div><p className='text-md mt-5 leading-tight'>{data.description}</p></div>
 
 <div className=' relative flex justify-between items-center py-[8px] w-full mt-[15px]'> 
-        <h5>.9MB</h5>
+        <h5>{data.filesize}</h5>
         <span className='w-[1.6rem] h-[1.6rem] bg-zinc-500 rounded-full flex items-center justify-center'>
-        <FaCloudDownloadAlt size=".85rem" color='#fff'/>
+            {data.close ? <IoCloseOutline size=".85rem" color='#fff'/>
+: <FaCloudDownloadAlt size=".85rem" color='#fff'/>}
 
         </span>
 
     </div>
     </div>
-<div className='footer absolute bottom-0 bg-sky-700 left-0 w-full h-[60px] flex items-center justify-center'>
-    <h3 className='font-semibold text-zinc-300'>Download Now</h3>
-</div>
+    {
+        data.tag.isOpen ? (<div className={`footer absolute bottom-0 left-0 w-full h-[45px] flex items-center justify-center ${data.tag.color === 'blue' ? "bg-blue-600" :"bg-green-600" }`}>
+        <h3 className='font-semibold text-zinc-300'>{data.tag.downloaded ? " Downloaded Already " : "Download Now !"}</h3>
+    </div>) : null
+    }
+
        </div>
     </div>
   )
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Card'
 function Foreground() {
-  
+  const data =[
+{description :"Card number One....", filesize:"1.5 MB", close:true, tag:{isOpen:true,downloaded:true,color:"blue"}},
+{description :"Card number Two....", filesize:"2.5 MB", close:false, tag:{isOpen:true,downloaded:false,color:"green"}},
+{description :"Card number Three....", filesize:"4.5 MB", close:true, tag:{isOpen:true,downloaded:true,color:"blue"}},
+{description :"Card number Four....", filesize:"6.5 MB", close:false, tag:{isOpen:true,downloaded:false,color:"blue"}}
+
+  ];
+
   return (
     <div>
-            <div className='w-full h-screen fixed z-[3] fixed top-0 left-0'>
-             <Card/>
+            <div className='w-full h-screen fixed z-[3] fixed top-0 left-0 flex gap-5 flex-wrap p-[20px]'>
+            {data.map((item,index)=>(
+              <Card data={item}/>
+            ))}
             </div>
-
     </div>
   )
 }
